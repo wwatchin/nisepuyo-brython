7b90ff50a0b22bbe0d0b1e80e0ac84a8fbb2b9eb
kWatanabe <68700263+wwatchin@users.noreply.github.com>
2021-08-29 10:03:30 +0900

    デバッグコードの追加とFieldクラスの追加

--- src/main.py
+++ src/main.py
@@ -1,42 +1,81 @@
 from browser import document, html
-import math
-import random
 
-canvas = document["drawarea"]
-context = canvas.getContext("2d")
+# debug
+debug = document["debugarea"]
+def msg (s):
+    debug.text = str(s)
 
-x = 10
-y = 5
+# Size and position
+STONE_SIZE = 32
+NR_ROW = 6
+NR_COLUMN = 12
+FIELD_WIDTH = NR_ROW * STONE_SIZE
+FIELD_HEIGHT = NR_COLUMN * STONE_SIZE
 
-# Images
-images_stone = (html.IMG(src="img/stone1.png"), html.IMG(src="img/stone2.png"),
-                html.IMG(src="img/stone3.png"), html.IMG(src="img/stone4.png"),
-                html.IMG(src="img/stone5.png"))
-# WASD
+# WASD + SPACE
 CHARCODE_UP = 119
 CHARCODE_LEFT = 97
 CHARCODE_DOWN = 115
 CHARCODE_RIGHT = 100
 CHARCODE_SPACE = 32
 
-def draw ():
-    context.clearRect(0, 0, 800, 640)
-    context.drawImage(images_stone[0], x, y)
+# Field
+class Field ():
+    def __init__ (self, context, images_stone, posx = 0, posy = 0):
+        self.__x = posx
+        self.__y = posy
+        self.__endx = posx + FIELD_WIDTH
+        self.__endy = posy + FIELD_HEIGHT
+        self.__context = context
+        self.__images_stone = images_stone
+        self.clear()
+
+    def draw(self):
+        self.__context.clearRect(self.__x, self.__y, self.__endx, self.__endy)
+        y = self.__y
+        for row in self.field:
+            x = self.__x
+            for cell in row:
+                if cell != None:
+                    self.__context.drawImage(self.__images_stone[cell], x, y)
+                x += STONE_SIZE
+            y += STONE_SIZE
+        msg(self.field)
+
+    def clear(self):
+        self.field = [[None] * NR_ROW for i in range(NR_COLUMN)]
+
+###
+### Materials.
+###
+images_stone = (html.IMG(src="img/stone0.png"), html.IMG(src="img/stone1.png"),
+                html.IMG(src="img/stone2.png"), html.IMG(src="img/stone3.png"),
+                html.IMG(src="img/stone4.png"), html.IMG(src="img/stone5.png"))
+
+###
+### Main routine.
+###
+canvas = document["drawarea"]
+context = canvas.getContext("2d")
+field = Field(context, images_stone)
 
 def cursol (event):
-    global x
-    global y
+    debug.text = "a"
     if event.charCode == CHARCODE_UP:
-        y -= 1
+        field.field[0][0] = 0
+        pass
     elif event.charCode == CHARCODE_DOWN:
-        y += 1
+        field.field[1][0] = 1
+        pass
     elif event.charCode == CHARCODE_RIGHT:
-        x += 1
+        field.field[1][1] = 2
+        pass
     elif event.charCode == CHARCODE_LEFT:
-        x -= 1
+        field.field[1][2] = 3
+        pass
     elif event.charCode == CHARCODE_SPACE:
-        x = 0
-        y = 0
-    draw()
+        field.field[2][2] = 4
+        pass
+    field.draw()
 
 document.bind("keypress", cursol)
